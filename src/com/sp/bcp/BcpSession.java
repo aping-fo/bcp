1af76d276b935d708602a5cd1c40edbdffc910cb
aping.foo <125455364@qq.com>
2017-04-08 15:48:59 +0800

    增加确认包大小

--- src/com/sp/bcp/BcpSession.java
+++ src/com/sp/bcp/BcpSession.java
@@ -8,7 +8,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class BcpSession {
 	/** 重传时间间隔，单位毫秒，下一次重传为RTO*2，以2的指数增加重传时间 */
-	private static final int RTO = 200; 
+	private static final int RTO = 200;
+	/**
+	 * 最多缓存多少个离线包，实际可以用配置，在这里直接写死
+	 */
+	private static final int MaxOfflinePack = 500;
+	
 	private Channel channel; // 提供多个连接?
 	private Map<Integer, Packet> packetMap = new ConcurrentHashMap<>();
 
@@ -31,7 +36,9 @@ public class BcpSession {
 	}
 
 	public void addPacket(Packet packet) {
-		packetMap.put(packet.seq, packet);
+		if(packetMap.size() < MaxOfflinePack) {
+			packetMap.put(packet.seq, packet);
+		}
 	}
 
 	public void removePacket(int id) {
@@ -58,8 +65,7 @@ public class BcpSession {
 			if (System.currentTimeMillis() > packet.time) {
 				send(packet);
 				packet.count += 1;
-				packet.time = System.currentTimeMillis() + RTO
-						* (1 << packet.count);
+				packet.time = System.currentTimeMillis() + RTO * (1 << packet.count);
 			}
 		}
 	}
