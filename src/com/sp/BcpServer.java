62d4a7e39dfa9b25f9c96a7207500091fc6ebdba
aping.foo <125455364@qq.com>
2017-04-11 09:39:52 +0800

    add main fuction

--- src/com/sp/BcpServer.java
+++ src/com/sp/BcpServer.java
@@ -1,5 +1,47 @@
 package com.sp;
 
-public class BcpServer {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
+import com.sp.bcp.BcpInitializer;
+
+public class BcpServer {
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			return;
+		}
+
+		int port = 9800;
+		try {
+			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			// TODO: logger exception
+		}
+		// Configure the server.
+		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(bossGroup, workerGroup)
+					.channel(NioServerSocketChannel.class)
+					.option(ChannelOption.SO_BACKLOG, 100)
+					.handler(new LoggingHandler(LogLevel.INFO))
+					.childHandler(new BcpInitializer());
+
+			// Start the server.
+			ChannelFuture f = b.bind(port).sync();
+			// Wait until the server socket is closed.
+			f.channel().closeFuture().sync();
+		} finally {
+			// Shut down all event loops to terminate all threads.
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
 }
